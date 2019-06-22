fabecf87b26d0071ef4dd8abb80eddc53c3b5c6d
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-06-22 16:31:05 +0800

    add : add php @return demo

--- php/return/index.php
+++ php/return/index.php
@@ -1,59 +1,39 @@
 <?php
 
-class ClassForReturn
+/**
+ * <p>"@return" : 用于在函数/方法返回值信息</p>
+ * <p>完整类型可查看README.md</p>
+ *
+ * <b>此标签建议在PHPStorm中打开此演示文档，可以看到具体的标签效果</b>
+ */
+class TagReturn
 {
-
-    public function hello() {}
-}
-
-class InstanceForReturn
-{
+    private static $config = [];
 
     /**
-     * @return mixed
+     * <1.> 返回主机名称
+     *
+     * @return string 主机名称
      */
-    public function methodWithReturnAll()
+    public static function getHost()
     {
+        return $_SERVER['host'] ?: '127.0.0.1';
     }
 
     /**
-     * @return string
+     * <2.> 返回配置信息，如果name为null，返回所有配置
+     *
+     * @param string|null $name 配置名称
+     * @return string|array 主机名称(返回值多类型)
      */
-    public function methodWithReturnString()
+    public static function getConfig($name = null)
     {
-    }
-
-    /**
-     * @return array
-     */
-    public function methodWithReturnArray()
-    {
-    }
-
-    /**
-     * @return ClassForReturn
-     */
-    public function methodWithReturnClass()
-    {
-    }
-
-    /**
-     * @return ClassForReturn|array
-     */
-    public function methodWithReturnMixed()
-    {
+        return isset(static::$config[$name]) ? static::$config[$name] : static::$config;
     }
 }
 
-$personInstance = new InstanceForReturn();
-
-// TODO
-$personInstance->methodWithReturnAll();
-// TODO
-$personInstance->methodWithReturnString();
-// TODO
-$personInstance->methodWithReturnArray();
-// TODO
-$personInstance->methodWithReturnClass();
-// TODO
-$personInstance->methodWithReturnMixed();
+$host = TagReturn::getHost();
+// PASS.
+strlen($host);
+// WARN. TagReturn::getHost返回值定义为string,所以$host用array方法会产生一个警告
+array_pop($host);
