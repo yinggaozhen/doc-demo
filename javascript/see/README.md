43a63bd9319c3b814c17f7576c00188148a383e1
Gaozhen Ying <yinggaozhen@hotmail.com>
2019-06-24 23:57:22 +0800

    add : add javascript @see demo

---
+++ javascript/see/README.md
@@ -0,0 +1,14 @@
+# @see
+
+`@see` : 此标签表示可以参考另一个标识符的说明文档，或者一个外部资源。
+
+**此标签推荐使用PhpStorm/WebStorm进行阅读,可以能直观体现标签的作用**
+
+## 语法
+
+- `@see <namepath>`
+- `@see <url>`
+
+## 标签效果
+
+![demo.jpg](./docs/demo1.gif)
---
+++ javascript/see/demo.js
@@ -0,0 +1,17 @@
+/**
+ * "@see" : URL跳转
+ *
+ * @constructor
+ * @see http://www.baidu.com
+ */
+function Foo() {
+    this.bar = null;
+}
+
+/**
+ * "@see" : 内部资源跳转
+ *
+ * @constructor
+ * @see {Foo#bar}
+ */
+function bar() {}
